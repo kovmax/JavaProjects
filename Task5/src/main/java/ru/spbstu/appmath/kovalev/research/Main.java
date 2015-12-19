905c27403645b1c8e2780f600bf41cb63be69157
maxkovalev03 <maxkovalev03@gmail.com>
2015-12-19 19:52:58 +0300

    Task5

--- Task5/src/main/java/ru/spbstu/appmath/kovalev/research/Main.java
+++ Task5/src/main/java/ru/spbstu/appmath/kovalev/research/Main.java
@@ -6,14 +6,42 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        Matrix m1 = new Matrix(15, 15);
+        Matrix m1 = new Matrix(2, 5);
         m1.createRandom(-200, 200);
-        Matrix m2 = new Matrix(15, 15);
+        Matrix m2 = new Matrix(5, 4);
         m2.createRandom(-100, 100);
 
-        final Research res = new Research(m1, m2);
-        Report report = res.doResearch(15 * 15);
-        File file = new File("report.txt");
+        Research res = new Research(m1, m2);
+        Report report = res.doResearch(2 * 4, 10);
+        File file = new File("Report 1 - Small matrix.txt");
+        try {
+            report.printReport(file);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        m1 = new Matrix(12, 5);
+        m1.createRandom(-200, 200);
+        m2 = new Matrix(5, 17);
+        m2.createRandom(-100, 100);
+
+        res = new Research(m1, m2);
+        report = res.doResearch(12 * 17, 10);
+        file = new File("Report 2 - Middle matrix.txt");
+        try {
+            report.printReport(file);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        m1 = new Matrix(30, 5);
+        m1.createRandom(-200, 200);
+        m2 = new Matrix(5, 30);
+        m2.createRandom(-100, 100);
+
+        res = new Research(m1, m2);
+        report = res.doResearch(30 * 30, 10);
+        file = new File("Report 3 - Big matrix.txt");
         try {
             report.printReport(file);
         } catch (Exception e) {
--- Task5/src/main/java/ru/spbstu/appmath/kovalev/research/Research.java
+++ Task5/src/main/java/ru/spbstu/appmath/kovalev/research/Research.java
@@ -3,6 +3,9 @@ package ru.spbstu.appmath.kovalev.research;
 import ru.spbstu.appmath.kovalev.Matrix;
 import ru.spbstu.appmath.kovalev.MatrixMultiply;
 
+import java.util.ArrayList;
+
+
 public class Research {
     private final Matrix m1;
     private final Matrix m2;
@@ -12,19 +15,55 @@ public class Research {
         this.m2 = m2;
     }
 
-    public Report doResearch(final int maxNumThreads) {
+    public Report doResearch(final int maxNumThreads, final int cIterations) {
         Report report = new Report();
-        for (int i = 1; i <= maxNumThreads; i++) {
-            try {
-                final long startTime = System.currentTimeMillis();
-                Matrix result = new MatrixMultiply(m1, m2, i).multiply();
-                final long executionTime = System.currentTimeMillis() - startTime;
-                String msg = "Count of threads - " + i + " Execution time - " + executionTime + " mls";
-                report.addMessage(msg);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+        long[][] values = new long[maxNumThreads][cIterations];
+        for (int j = 1; j <= cIterations; j++){
+            for (int i = 1; i <= maxNumThreads; i++) {
+                try {
+                    final long startTime = System.currentTimeMillis();
+                    Matrix result = new MatrixMultiply(m1, m2, i).multiply();
+                    final long executionTime = System.currentTimeMillis() - startTime;
+                    values[i - 1][j - 1] = executionTime;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }
+
+        double[] res = new double[maxNumThreads];
+        for (int i = 0; i < maxNumThreads; i++) {
+            for (int j = 0; j < cIterations; j++) {
+                res[i] += values[i][j];
+            }
+            res[i] /= cIterations;
+        }
+
+        double min = res[0];
+        for (int i = 1; i < maxNumThreads; i++) {
+            if (res[i] < min)
+                min = res[i];
+        }
+
+        ArrayList<Integer> threads = new ArrayList<>();
+        for (int i = 0; i < maxNumThreads; i++) {
+            if (res[i] == min)
+                threads.add(i + 1);
+        }
+
+        report.addMessage("Dimension of result matrix: " + m1.getRows() + " * " +  m2.getColumns());
+        String msg = "";
+        for (int i = 0; i < threads.size(); i++) {
+            msg += threads.get(i);
+            if (i + 1 != threads.size())
+                msg += ", ";
+        }
+        report.addMessage("Min time: " + min + " mls for count of threads: " + msg);
+        for (int i = 0; i < maxNumThreads; i++) {
+            msg = "Count of threads - " + (i + 1) + " Execution time - " + res[i] + " mls";
+            report.addMessage(msg);
+        }
+
         return report;
     }
 }
